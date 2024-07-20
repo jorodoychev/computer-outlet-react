6fdbd25afbc15d066cdd3240c561f4c14bfe3781
jorodoychev <jorodoychev@gmail.com>
2024-07-20 17:08:50 +0300

    initial commit

--- client/src/guards/AuthGuard.jsx
+++ client/src/guards/AuthGuard.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom"
 import AuthContext from "./../contexts/authContext"
 
 // eslint-disable-next-line no-unused-vars
-export default function AuthGuard(props) {
+export default function AuthGuard() {
     const { isAuthenticated } = useContext(AuthContext)
 
     if (!isAuthenticated) {
